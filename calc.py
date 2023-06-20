cee47d0700ed7611d2934e06ff874dcf130121a7
hernanbiondini <hernanbiondini@gmail.com>
2023-06-20 17:35:38 -0300

    modulos

--- calc.py
+++ calc.py
@@ -1,18 +1,4 @@
-# Esta función suma dos números
-def sumar(x, y):
-    return x + y
-
-# Esta función resta dos números
-def restar(x, y):
-    return x - y
-
-# Esta función multiplica dos números
-def multiplicar(x, y):
-    return x * y
-
-# Esta función divide dos números
-def dividir(x, y):
-    return x / y
+import calc_function
 
 print("Seleccione una operación:")
 print("1. Sumar")
@@ -32,14 +18,14 @@ if opcion in ('1', '2', '3', '4'):
         print("Entrada inválida. Por favor, ingrese un número.")
     else:
         if opcion == '1':
-            print(num1, "+", num2, "=", sumar(num1, num2))
+            print(num1, "+", num2, "=", calc_function.sumar(num1, num2))
         elif opcion == '2':
-            print(num1, "-", num2, "=", restar(num1, num2))
+            print(num1, "-", num2, "=", calc_function.restar(num1, num2))
         elif opcion == '3':
-            print(num1, "*", num2, "=", multiplicar(num1, num2))
+            print(num1, "*", num2, "=", calc_function.multiplicar(num1, num2))
         elif opcion == '4':
             if num2 != 0:
-                print(num1, "/", num2, "=", dividir(num1, num2))
+                print(num1, "/", num2, "=", calc_function.dividir(num1, num2))
             else:
                 print("Error: No se puede dividir entre cero.")
 else:
---
+++ calc_function.py
@@ -0,0 +1,15 @@
+# Esta función suma dos números
+def sumar(x, y):
+    return x + y
+
+# Esta función resta dos números
+def restar(x, y):
+    return x - y
+
+# Esta función multiplica dos números
+def multiplicar(x, y):
+    return x * y
+
+# Esta función divide dos números
+def dividir(x, y):
+    return x / y
