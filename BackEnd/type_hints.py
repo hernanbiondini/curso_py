12f205d3e510b74f961a0b924e0b6fe255962d6a
hernanbiondini <hernanbiondini@gmail.com>
2023-06-29 20:50:25 -0300

    type_hint

---
+++ BackEnd/type_hints.py
@@ -0,0 +1,19 @@
+### Type Hints ###
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 9
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable2: str = "My string variable"
+print(my_string_variable2)
+print(type(my_string_variable2))
+
+my_string_variable2 = 5
+print(type(my_string_variable2))
+
+
+
